bc32dab05a94f183ec8be4089c08affd2127e2c3
Afiq Amri <120144945+afiqamrii@users.noreply.github.com>
2026-01-29 21:28:42 +0800

    adjusting json conflict issues

--- acara-backend/routes/api.php
+++ acara-backend/routes/api.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\AuthController;
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
-=======
+
 Route::post('/login', [AuthController::class, 'Login']);
->>>>>>> login_daus
