05a7e040c0c45db0c41cbc2de537fe7b95ebeb7d
TuanFirdaus-S <tfirdaus676@gmail.com>
2026-02-08 00:48:21 +0800

    changes name M to marketplace and navbar code

--- acara-frontend/src/features/header/pages/navbar.tsx
+++ acara-frontend/src/features/header/pages/navbar.tsx
@@ -1,12 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-// import CartDrawer from "./cartdrawer";
+import CartDrawer from "./cartdrawer";
 import './navbar.css';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [scrolled, setScrolled] = useState(false);
-  //   const [cartOpen, setCartOpen] = useState(false);
+  const [cartOpen, setCartOpen] = useState(false);
 
   const token = localStorage.getItem("token");
   const userName = localStorage.getItem("user_name");
@@ -46,19 +46,19 @@ const Navbar = () => {
         <button onClick={() => navigate("/")}>Home</button>
         <button onClick={() => navigate("/marketplace")}>Marketplace</button>
 
-        {/* <button className="cart-btn" onClick={() => setCartOpen(true)}>
+        <button className="cart-btn" onClick={() => setCartOpen(true)}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <circle cx="9" cy="21" r="1" />
             <circle cx="20" cy="21" r="1" />
-            <path d="M1 1h4l2.6 13h10.4l2-8H6" />
+            <path d="M1 1h4l2.6 13h10.4l2-8H6" /> 
           </svg>
           <span className="cart-badge">0</span>
-        </button> */}
+        </button>
       </div>
-      {/* <CartDrawer open={cartOpen} onClose={() => setCartOpen(false)} /> */}
+      <CartDrawer open={cartOpen} onClose={() => setCartOpen(false)} />
     </nav>
-
+    
   );
 };
 
