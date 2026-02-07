84237215ef9d9adddf1098dcfba62f05ba53d616
TuanFirdaus-S <tfirdaus676@gmail.com>
2026-02-08 00:40:18 +0800

    marketplace route on app.tsx

--- acara-frontend/src/App.tsx
+++ acara-frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/register/Register";
 import UserDashboard from './features/dashboard/pages/UserDashboard';
 import Navbar from './features/header/pages/navbar';
-// import Marketplace from './features/marketplace/pages/marketplace';
+import Marketplace from './features/marketplace/pages/marketplace';
 import LandingPage from './features/landing_page/pages/LandingPage';
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
           <Route path="/dashboard" element={<div>Vendor Dashboard</div>} />
           <Route path="/crew/jobs" element={<div>Crew Jobs</div>} />
           <Route path="/admin/panel" element={<div>Admin Panel</div>} />
-          {/* <Route path="/marketplace" element={<Marketplace />} /> */}
+          <Route path="/marketplace" element={<Marketplace />} />
         </Routes>
       </div>
     </BrowserRouter>
